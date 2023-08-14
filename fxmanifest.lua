16174f2b8d574386cddd86adf29a1f362648b97d
Alexander <141943814+XyraL@users.noreply.github.com>
2023-08-14 01:12:50 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'South Coast Customs'
+
+version '0.1.0'
+
+files {
+    "data/**/*.meta",
+}
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+
